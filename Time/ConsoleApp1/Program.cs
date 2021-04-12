03f44955a3577fb1131499ec6bc0720cc8e3ea72
Nekro <80943379+pzps94038@users.noreply.github.com>
2021-04-12 14:47:06 +0800

    Add files via upload

---
+++ Time/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // poe是 timeinfo的新物件 
+            TimeInfo poe = new TimeInfo();
+            // 呼叫 timeinfo裡面的方法showtime,poe套入參數18顯示晚上六點
+            poe.ShowTime(18);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Time/ConsoleApp1/TimeInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class TimeInfo
+    {
+        public TimeInfo() { Console.WriteLine("呼叫時間"); }
+        ~TimeInfo() { Console.WriteLine("釋放資源"); }
+        public int Hrs { get; set; }
+        public void ShowTime(int tm)
+        {
+            Hrs = tm;
+            if (Hrs > 12)
+            {
+                Hrs %= 12;
+                Console.WriteLine($"時間是下午: {Hrs} 點");
+            }
+            else Console.WriteLine($"時間是上午: {Hrs} 點");
+        }
+
+    }
+}
